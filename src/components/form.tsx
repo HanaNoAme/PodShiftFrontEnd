86884bffa53d5b1e27e0aabce6c7f59556e18ed8
Ame <amelie0417@live.be>
2024-09-02 20:40:15 -0400

    gt

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -8,7 +8,6 @@ enum Recurrence {
 }
 
 export function Form() {
-  const [isFormEmpty, setIsFormEmpty] = useState(true);
   const [form, setForm] = useState({
     url: '',
     episodes: 1,
@@ -16,26 +15,22 @@ export function Form() {
     frequence: 1
   });
 
-  function handleChange(e: React.ChangeEvent<any>) {
-    console.log('Event : ', e.target);
-    console.log('Form : ', form);
-    setIsFormEmpty(false);
-  };
-
-  function handleUrlChange(e: React.ChangeEvent<HTMLInputElement>) {
-    // /^(https?:\/\/)(www.)([a-z0-9.-]+)?(\.[a-z]{2,})(\/[a-z0-9.-]*)*\/?(feed|rss|rss\.xml|index\.xml|index\.rss|feed\.rss|feed\.xml)?\/?$/gm
-    setForm({ ...form, url: e.target.value });
+  function handleUrlBlur(e: React.ChangeEvent<HTMLInputElement>) {
+    var url = e.target.value;
+    console.log(`URL: ${url}`);
+    if (url.match("^[a-zA-Z ]*$") != null) setForm({ ...form, url: url });
+    else console.log("");
   }
 
-  function handleEpisodesChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleEpisodesBlur(e: React.ChangeEvent<HTMLInputElement>) {
     setForm({ ...form, episodes: parseInt(e.target.value) });
   }
 
-  function handleRecurrenceChange(e: React.ChangeEvent<HTMLSelectElement>) {
+  function handleRecurrenceBlur(e: React.ChangeEvent<HTMLSelectElement>) {
     setForm({ ...form, recurrence: parseInt(e.target.value) });
   }
 
-  function handleFrequenceChange(e: React.ChangeEvent<HTMLInputElement>) {
+  function handleFrequenceBlur(e: React.ChangeEvent<HTMLInputElement>) {
     setForm({ ...form, frequence: parseInt(e.target.value) });
   }
 
@@ -45,25 +40,25 @@ export function Form() {
   }
 
   return (
-    <form onChange={handleChange} onSubmit={e => {
+    <form onSubmit={e => {
       e.preventDefault();
       onSubmit();
     }}>
+      {}
       <input type="reset" value="Clear all fields" onClick={() => {
         alert('Are you sure you want to clear all fields?');
-      }} disabled={isFormEmpty} />
+      }} />
       <br />
       <br />
-      <label>URL : <input type="text" value={form.url} onChange={handleUrlChange}
-        pattern="^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$" />
+      <label>URL : <input type="text" onBlur={handleUrlBlur} />
       </label>
       <br />
       <br />
-      <label>Number of Episodes : <input type="number" min="1" value={form.episodes} defaultValue={1} onChange={handleEpisodesChange} />
+      <label>Number of Episodes : <input type="number" min="1" defaultValue={1} onBlur={handleEpisodesBlur} />
       </label>
       <br />
       <br />
-      <label>Recurrence : <select onChange={handleRecurrenceChange}>
+      <label>Recurrence : <select onBlur={handleRecurrenceBlur}>
           {Object.keys(Recurrence).map(key => (
             <option key={key} value={key}>
               {Recurrence[key as keyof typeof Recurrence]}
@@ -73,7 +68,7 @@ export function Form() {
       </label>
       <br />
       <br />
-      <label>Frequence : <input type="number" min="1" defaultValue={1} onChange={handleFrequenceChange} />
+      <label>Frequence : <input type="number" min="1" defaultValue={1} onBlur={handleFrequenceBlur} />
       </label>
       <br />
       <br />
