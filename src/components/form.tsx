4b403cc1ddc3b373164c5eb7a5babe00930dfd25
Amélie <amelie0417@live.be>
2024-11-10 20:57:28 -0500

    validation done

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState } from "react";
+import validator from "validator";
 
 enum Recurrence {
   Yearly = "year",
@@ -8,60 +9,66 @@ enum Recurrence {
 }
 
 export function CustomForm() {
-  const [isFormValid, setIsFormValid] = useState(false);
   const [form, setForm] = useState({
     url: "",
     episodes: 1,
     frequence: 1,
     recurrence: 3,
   });
-
-  // function validateForm) {
-  //   if (!form.url) {
-  //     setIsFormValid(false);
-  //     console.log("Missing URL");
-  //     return;
-  //   }
-  //   if (form.url.match("https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256} [a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)") != null) {
-  //     setIsFormValid(true);
-  //     console.log("Good URL");
-  //   } else {
-  //     console.log("Bad URL");
-  //     setIsFormValid(false);
-  //     return;
-  //   }
-
-  //   if (!form.episodes || !form.frequence)
-  //     setIsFormValid(false);
-  // }
+  const [isInputValid, setIsInputValid] = useState({
+    url: false,
+    episodes: true,
+    frequence: true,
+    recurrence: true,
+  });
+  const [hasSelectedUrlField, setHasSelectedUrlField] = useState(false);
+  const isFormValid = Object.values(isInputValid).every(Boolean);
 
   function handleUrlBlur(e: React.FocusEvent<HTMLInputElement>) {
+    if (!hasSelectedUrlField) setHasSelectedUrlField(true);
     var url = e.target.value;
-    console.log(`URL: ${url}`);
-    if (url.match("^[a-zA-Z ]*$") != null) {
-      setForm({ ...form, url: url })
-    }
-    else {
-      //TODO: validation error
-    }
+    
+    if (validateInput(url)) {
+      setForm({ ...form, url: url });
+      setIsInputValid({ ...isInputValid, url: true });
+    } else
+      setIsInputValid({ ...isInputValid, url: false });
   }
 
   function handleEpisodesChange(e: React.ChangeEvent<HTMLInputElement>) {
     var episodes = parseInt(e.target.value);
-    console.log(`episodes: ${episodes}`);
-    setForm({ ...form, episodes: episodes });
+    
+    if (validateInput(episodes)) {
+      setForm({ ...form, episodes: episodes });
+      setIsInputValid({ ...isInputValid, episodes: true });
+    } else
+      setIsInputValid({ ...isInputValid, episodes: false });
   }
 
   function handleFrequenceChange(e: React.ChangeEvent<HTMLInputElement>) {
     var frequence = parseInt(e.target.value);
-    console.log(`frequence: ${frequence}`);
-    setForm({ ...form, frequence: frequence });
+
+    if (validateInput(frequence)) {
+      setForm({ ...form, frequence: frequence });
+      setIsInputValid({ ...isInputValid, frequence: true });
+    } else
+      setIsInputValid({ ...isInputValid, frequence: false });
   }
 
   function handleRecurrenceChange(e: React.ChangeEvent<HTMLSelectElement>) {
-    var index = Object.keys(Recurrence).indexOf(e.target.value);
-    console.log(`recurrence: ${index}`);
-    setForm({ ...form, recurrence: index });
+    var recurrence = Object.keys(Recurrence).indexOf(e.target.value);
+    
+    if (validateInput(recurrence + 1)) { // because index starts at 0
+      setForm({ ...form, recurrence: recurrence });
+      setIsInputValid({ ...isInputValid, recurrence: true });
+    } else
+      setIsInputValid({ ...isInputValid, recurrence: false });
+  }
+
+  function validateInput(value: string | number): boolean {
+    if (typeof value === "string") return validator.isURL(value);
+    if (typeof value === "number") return value > 0;
+    return false;
   }
 
   function handleClear() { 
@@ -71,6 +78,12 @@ export function CustomForm() {
         episodes: 1,
         frequence: 1,
         recurrence: 3,
+      });
+      setIsInputValid({
+        url: false,
+        episodes: true,
+        frequence: true,
+        recurrence: true,
       });
     }
   }
@@ -82,30 +95,32 @@ export function CustomForm() {
     console.log(`frequence: ${form.frequence}`);
     console.log(`recurrence: ${form.recurrence}`);
     //TODO : Send request to server
-
   }
 
   return (
     <form className="text-start translate-middle-x start-50 position-relative w-75 my-5" onSubmit={handleSubmit} noValidate>
       <div>
-        <label className="form-label">URL</label>
-        <input type="text" className="form-control" onBlur={handleUrlBlur} required />
+        <label htmlFor="url" className="form-label">URL</label>
+        <input id="url" type="text" className="form-control" onBlur={handleUrlBlur} required />
+        { !isInputValid.url && hasSelectedUrlField &&
+          <div className="text-danger">
+            Invalid URL
+          </div>
+        }
       </div>
       <br />
       <div className="text-start row">
         <div className="col">
-          <label className="form-label">Number of Episodes</label>
-          <input type="number" className="form-control" min="1" defaultValue={1}
-            onChange={handleEpisodesChange} required />
+          <label htmlFor="episodes" className="form-label">Number of Episodes</label>
+          <input id="episodes" type="number" className="form-control" min="1" defaultValue={1} onChange={handleEpisodesChange} required />
         </div>
         <div className="col align-self-end">
-          <label className="form-label">Frequence</label>
-          <input type="number" className="form-control" min="1" defaultValue={1}
-            onChange={handleFrequenceChange} required />
+          <label htmlFor="frequence" className="form-label">Frequence</label>
+          <input id="frequence" type="number" className="form-control" min="1" defaultValue={1} onChange={handleFrequenceChange} required />
         </div>
         <div className="col align-self-end">
-          <label className="form-label">Recurrence</label>
-          <select className="form-control" onChange={handleRecurrenceChange} required>
+          <label htmlFor="recurrence" className="form-label">Recurrence</label>
+          <select id="recurrence" className="form-control" onChange={handleRecurrenceChange} required>
             {Object.keys(Recurrence).reverse().map(key => (
               <option key={key} value={key}>
                 {key}
@@ -117,7 +132,7 @@ export function CustomForm() {
       <br />
       <p>{form.episodes} episode{form.episodes > 1 ? "s" : ""} every {form.frequence > 1 ? `${form.frequence} ` : ""}{Object.values(Recurrence)[form.recurrence]}{form.frequence > 1 ? "s" : ""}</p>
       <div className="d-flex justify-content-between">
-        <button type="submit" className="btn btn-primary" /*disabled={!isFormValid}*/ >
+        <button type="submit" className="btn btn-primary" disabled={!isFormValid} >
           Submit
         </button>
         <button type="reset" className="btn btn-secondary" onClick={handleClear}>
