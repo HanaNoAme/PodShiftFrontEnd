380c08aa659fb8606c912708d69c78b25f914224
Amélie <amelie0417@live.be>
2024-11-23 23:25:37 -0500

    Squashed commit of the following:
    commit 17855e60fbccf595f4f1c47319e887519bfb68f3
    Author: Amélie <amelie0417@live.be>
    Date:   Sat Nov 23 23:01:44 2024 -0500
    
        input done
    
    commit 650b6023178c450edbfc5f057839b435d2b78854
    Author: Amélie <amelie0417@live.be>
    Date:   Fri Nov 22 10:35:19 2024 -0500
    
        progress
    
    commit 9cc4c14bf1294bdfd02988b6f04b99e1493085a6
    Author: Amélie <amelie0417@live.be>
    Date:   Mon Nov 18 01:04:43 2024 -0500
    
        wip input

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,22 +1,39 @@
 import { useState } from "react";
-import validator from "validator";
-import { Result } from './result'
+import { Input } from "./input";
+import { Result } from './result';
+
+export interface Field {
+  value: string | number
+  isValid: boolean
+}
 
 interface FormState {
-  url: string;
-  episodes: number;
-  frequence: number;
-  recurrence: number;
+  url: Field
+  episodes: Field
+  frequence: Field
+  recurrence: Field
 }
 
-interface FormValidation {
-  url: boolean;
-  episodes: boolean;
-  frequence: boolean;
-  recurrence: boolean;
-}
+const initialForm: FormState = {
+  url: {
+    value: "",
+    isValid: false,
+  },
+  episodes: {
+    value: 1,
+    isValid: true,
+  },
+  frequence: {
+    value: 1,
+    isValid: true,
+  },
+  recurrence: {
+    value: 3,
+    isValid: true,
+  },
+};
 
-enum Recurrence {
+export enum Recurrence {
   Yearly = "year",
   Monthly = "month",
   Weekly = "week",
@@ -24,85 +41,22 @@ enum Recurrence {
 }
 
 export function Form() {
-  const INITIAL_FORM_STATE: FormState = {
-    url: "",
-    episodes: 1,
-    frequence: 1,
-    recurrence: 3
-  };
-  const INITIAL_VALIDATION_STATE: FormValidation = {
-    url: false,
-    episodes: true,
-    frequence: true,
-    recurrence: true
-  };
-  const [form, setForm] = useState(INITIAL_FORM_STATE)
-  const [isInputValid, setIsInputValid] = useState(INITIAL_VALIDATION_STATE);
-  const [hasSelectedUrlField, setHasSelectedUrlField] = useState(false);
-  const isFormValid = Object.values(isInputValid).every(Boolean);
-
-  function handleUrlBlur(e: React.FocusEvent<HTMLInputElement>) {
-    if (!hasSelectedUrlField) setHasSelectedUrlField(true);
-    var url = e.target.value;
-
-    if (validateInput(url)) {
-      setForm({ ...form, url: url });
-      setIsInputValid({ ...isInputValid, url: true });
-    } else
-      setIsInputValid({ ...isInputValid, url: false });
-  }
-
-  function handleEpisodesChange(e: React.ChangeEvent<HTMLInputElement>) {
-    var episodes = parseInt(e.target.value);
-
-    if (validateInput(episodes)) {
-      setForm({ ...form, episodes: episodes });
-      setIsInputValid({ ...isInputValid, episodes: true });
-    } else
-      setIsInputValid({ ...isInputValid, episodes: false });
-  }
-
-  function handleFrequenceChange(e: React.ChangeEvent<HTMLInputElement>) {
-    var frequence = parseInt(e.target.value);
-
-    if (validateInput(frequence)) {
-      setForm({ ...form, frequence: frequence });
-      setIsInputValid({ ...isInputValid, frequence: true });
-    } else
-      setIsInputValid({ ...isInputValid, frequence: false });
-  }
-
-  function handleRecurrenceChange(e: React.ChangeEvent<HTMLSelectElement>) {
-    var recurrence = Object.keys(Recurrence).indexOf(e.target.value);
-
-    if (validateInput(recurrence + 1)) { // because index starts at 0
-      setForm({ ...form, recurrence: recurrence });
-      setIsInputValid({ ...isInputValid, recurrence: true });
-    } else
-      setIsInputValid({ ...isInputValid, recurrence: false });
-  }
-
-  function validateInput(value: string | number): boolean {
-    if (typeof value === "string") return validator.isURL(value);
-    if (typeof value === "number") return value > 0;
-    return false;
-  }
+  const [form, setForm] = useState(initialForm)
+  const isFormValid = Object.values(form).every((field) => field.isValid);
 
   function handleClear() {
     if (!confirm("Are you sure you want to clear all fields?")) return;
-    setForm(INITIAL_FORM_STATE);
-    setIsInputValid(INITIAL_VALIDATION_STATE);
+    setForm(initialForm);
   }
 
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
+    console.log(form);
 
     if (!isFormValid) {
       alert("Form is invalid. Please check your inputs.");
       return;
     }
-
-    console.log(form);
 
     try {
       const response = await fetch('http://www.podshift.net:8080/PodShift', {
@@ -110,15 +64,17 @@ export function Form() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(form)
+        body: JSON.stringify(Object.fromEntries(
+          Object.entries(form).map(([key, value]) => [key, value.value])
+        ))
       });
 
       if (response.ok) {
         const data = await response.json();
         console.log(data);
-        alert('Schedule created successfully!');
+        alert('Success!');
       } else {
-        throw new Error('Failed to create schedule');
+        throw new Error('Failed');
       }
     } catch (error: any) {
       alert('Error creating schedule: ' + error.message);
@@ -128,38 +84,39 @@ export function Form() {
   return (
     <>
       <form className="text-start translate-middle-x start-50 position-relative w-75 my-5" onSubmit={handleSubmit} noValidate>
-        <div>
-          <label htmlFor="url" className="form-label">URL</label>
-          <input id="url" type="text" className="form-control" onBlur={handleUrlBlur} required />
-          {!isInputValid.url && hasSelectedUrlField &&
-            <div className="text-danger">
-              Invalid URL
-            </div>
-          }
-        </div>
+        <Input
+          name="url"
+          display="URL"
+          type="text"
+          field={form.url}
+          setInputValue={(value, isValid) => 
+            setForm((prev) => ({ ...prev, url: { value, isValid } }))} />
         <br />
         <div className="text-start row">
-          <div className="col">
-            <label htmlFor="episodes" className="form-label">Number of Episodes</label>
-            <input id="episodes" type="number" className="form-control" min="1" defaultValue={1} onChange={handleEpisodesChange} required />
-          </div>
-          <div className="col align-self-end">
-            <label htmlFor="frequence" className="form-label">Frequence</label>
-            <input id="frequence" type="number" className="form-control" min="1" defaultValue={1} onChange={handleFrequenceChange} required />
-          </div>
-          <div className="col align-self-end">
-            <label htmlFor="recurrence" className="form-label">Recurrence</label>
-            <select id="recurrence" className="form-control" onChange={handleRecurrenceChange} required>
-              {Object.keys(Recurrence).reverse().map(key => (
-                <option key={key} value={key}>
-                  {key}
-                </option>
-              ))}
-            </select>
-          </div>
+          <Input
+            name="episodes"
+            display="Number of Episodes"
+            type="number"
+            field={form.episodes}
+            setInputValue={(value, isValid) =>
+              setForm((prev) => ({ ...prev, episodes: { value, isValid } }))} />
+          <Input
+            name="frequence"
+            display="Frequence"
+            type="number"
+            field={form.frequence}
+            setInputValue={(value, isValid) =>
+              setForm((prev) => ({ ...prev, frequence: { value, isValid } }))} />
+          <Input
+            name="recurrence"
+            display="Recurrence"
+            type="select"
+            field={form.recurrence}
+            setInputValue={(value, isValid) =>
+              setForm((prev) => ({ ...prev, recurrence: { value, isValid } }))} />
         </div>
         <br />
-        <p>{form.episodes} episode{form.episodes > 1 ? "s" : ""} every {form.frequence > 1 ? `${form.frequence} ` : ""}{Object.values(Recurrence)[form.recurrence]}{form.frequence > 1 ? "s" : ""}</p>
+        <p>{form.episodes.value} episode{Number(form.episodes.value) > 1 ? "s" : ""} every {Number(form.frequence.value) > 1 ? `${form.frequence.value} ` : ""}{Object.values(Recurrence)[Number(form.recurrence.value)]}{Number(form.frequence.value) > 1 ? "s" : ""}</p>
         <div className="d-flex justify-content-between">
           <button type="submit" className="btn btn-primary" disabled={!isFormValid} >
             Submit
---
+++ src/components/input.tsx
@@ -0,0 +1,73 @@
+import validator from "validator";
+import { useState } from "react";
+import { Field, Recurrence } from "./form";
+
+interface Props {
+  name: string
+  display: string
+  type: "text" | "number" | "select"
+  field: Field
+  setInputValue: (value: string | number, isValid: boolean) => void
+}
+
+export function Input(props: Props) {
+  var [isUrlTouched, setIsUrlTouched] = useState(false)
+
+  function handleInputBlur(e: React.FocusEvent<HTMLInputElement>) {
+    if (props.name == "url" && !isUrlTouched) setIsUrlTouched(true);
+    setValue(e.target.value);
+  }
+
+  function handleSelectChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    var value = Object.keys(Recurrence).indexOf(e.target.value);
+    setValue(value);
+  }
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    var value = parseInt(e.target.value);
+    setValue(value);
+  }
+
+  function setValue(value: string | number) {
+    const isValid = validateInput(value);
+    props.setInputValue(isValid ? value : props.field.value, isValid);
+  }
+
+  function validateInput(value: string | number): boolean {
+    if (typeof value === "string" && props.name == "url") return validator.isURL(value);
+    if (typeof value === "number") {
+      if (props.name == "recurrence") value += 1; // Recurrence index starts at 0
+      return value > 0;
+    }
+    return false;
+  }
+
+  const renderInput = () => {
+    if (props.type == "select") {
+      return (
+        <select id={props.name} className="form-control" onChange={handleSelectChange} required>
+          {Object.keys(Recurrence).reverse().map(key => (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          ))}
+        </select>
+      );
+    }
+
+    return (
+      <input id={props.name} type={props.type} className="form-control"
+        {...(props.type == "text" ? { onBlur: handleInputBlur } : { onChange: handleInputChange })} min="1" defaultValue={props.type == "text" ? "" : 1} required />
+    );
+  };
+
+  return (
+    <div className={props.type == "text" ? "" : "col align-self-end"}>
+      <label htmlFor={props.name} className="form-label">{props.display}</label>
+      {renderInput()}
+      {!props.field.isValid && isUrlTouched &&
+        <div className="text-danger">Invalid URL</div>
+      }
+    </div>
+  );
+}
